0b5a4ccd5b78cbd6267a2922343369e75a00bc48
Mike Echlin <mikeechlin@gmail.com>
2017-03-18 20:42:47 +0000

    no submit

--- server.js
+++ server.js
@@ -6,27 +6,27 @@ var express = require('express');
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
 
-var router = express();
-var app = express();
-var server = http.createServer(router);
- 
-router.use(express.static(path.resolve(__dirname, 'client')));
 
-//app.post('/profile', upload.single('avatar'), function (req, res, next) {
+var app = express();
+var server = http.createServer(app);
+ 
+app.use(express.static(path.resolve(__dirname, 'client')));
+
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
   // req.file is the `avatar` file 
   // req.body will hold the text fields, if there were any 
-//});
+});
  
-//app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
+app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
   // req.files is array of `photos` files 
   // req.body will contain the text fields, if there were any 
-//});
+});
  
-// app.get('*', function (req,res){
-//   res.sendFile('../client/index.html');
-// });
+app.get('*', function (req,res){
+  res.sendFile('../client/index.html');
+});
  
-//var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }]);
+var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }]);
 app.post('/cool-profile', function (req, res, next) {
   // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files 
   // 
